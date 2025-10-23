db3c7998f578439bc2c03f578a8614dc6f976afc
ola <aleksandra.dancewicz@interia.pl>
2025-10-23 15:49:51 +0200

    Revert "upload lab11 - add cookie-based shopping cart"
    This reverts commit 5da360698765edf5e1f41ef3e7b3c59ed3732937.

--- ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace lista10.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
